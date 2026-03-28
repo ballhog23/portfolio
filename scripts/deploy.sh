26c1724bb6903f47b403860ab02fb9d0494e363d
caleb <calebpirkle2@gmail.com>
2026-03-28 17:05:36 -0500

    try without health check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,3 @@ docker pull $ECR_URI:latest
 docker stop calebpirklesite || true
 docker rm calebpirklesite || true
 docker run --name calebpirklesite -d -p 80:80 -p 443:443 -v caddy_data:/data --restart unless-stopped $ECR_URI:latest
-for i in 1 2 3 4 5; do
-  sleep 3
-  curl -fsk --resolve calebpirkle.com:443:127.0.0.1 https://calebpirkle.com && break
-  [ $i -eq 5 ] && exit 1
-done
