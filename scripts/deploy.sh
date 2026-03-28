8410aebc39f4a44e971718fd51123021b8305b4a
caleb <calebpirkle2@gmail.com>
2026-03-28 17:02:39 -0500

    caddy not looking for localhost

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,6 @@ docker rm calebpirklesite || true
 docker run --name calebpirklesite -d -p 80:80 -p 443:443 -v caddy_data:/data --restart unless-stopped $ECR_URI:latest
 for i in 1 2 3 4 5; do
   sleep 3
-  curl -fsk https://localhost && break
+  curl -fsk --resolve calebpirkle.com:443:127.0.0.1 https://calebpirkle.com && break
   [ $i -eq 5 ] && exit 1
 done
